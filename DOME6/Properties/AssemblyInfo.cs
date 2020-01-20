99fc5002b8e5dd72bc3b5339139419b972f999a7
Garrett Arnhold <garrett@arnholdsystems.com>
2020-01-19 22:22:53 -0800

    Added Reference to CrunchySodium

--- DOME6/Properties/AssemblyInfo.cs
+++ DOME6/Properties/AssemblyInfo.cs
@@ -21,6 +21,7 @@ using Crunchy.Noodle;
     typeof(Crunchy.Dough.ThisAssembly),
     typeof(Crunchy.Salt.ThisAssembly),
     typeof(Crunchy.Noodle.ThisAssembly),
+    typeof(Crunchy.Sodium.ThisAssembly),
     typeof(Crunchy.Ginger.ThisAssembly),
     typeof(Crunchy.Bun.ThisAssembly),
     typeof(Crunchy.Lunch.ThisAssembly)
